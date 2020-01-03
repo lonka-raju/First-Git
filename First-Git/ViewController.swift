33d510bd1fa429704844652d5ac29f3c4d311374
raju_lonka <raju@quadrantsystems.com>
2020-01-03 19:20:47 +0530

    method added.

--- First-Git/ViewController.swift
+++ First-Git/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("First - git pushed & commited..")
+        
+        self.callingFun()
+    }
+    
+    func callingFun(){
+        print("Hell world")
     }
     
 }
